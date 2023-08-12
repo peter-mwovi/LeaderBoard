f82e3b1ba4c3719de1415742c10f255c82aee1cd
mwovi <petermwovi73@gmail.com>
2023-08-12 21:06:00 +0300

    Add Source files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import './style.css';
 import getInput from './Modules/input';
 import ScoreBoard from './Modules/board';
+import createGame from './Modules/createGame';
 
-const Scores = [];
+createGame();
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#name').value.trim();
+  const score = document.querySelector('#score').value.trim();
+  if (name === '' || score === '' || Number.isNaN(score)) {
+    return;
+  }
+  getInput();
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
+});
 
-getInput(Scores);
-document.addEventListener('DOMContentLoaded', ScoreBoard(Scores));
+document.getElementById('refresh').addEventListener('click', (e) => {
+  e.preventDefault();
+  ScoreBoard();
+});
